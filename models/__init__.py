2b98febda364fb3ef96ff9f803c8d9bc1a2c0dfe
rottenbutcher <107749463+rottenbutcher@users.noreply.github.com>
2025-10-31 15:45:31 +0900

    Refactor ReID backbones into dedicated modules

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,8 @@
 from .build import build_model_from_cfg
+
 import models.Point_MAE
 import models.Point_PCP_MAE
 import models.Point_M2AE
+import models.point_transformer_reid
+import models.dgcnn_reid
+import models.pointnext_reid
